3a5a1d9d258ef40c7b5b4c5303032376644560d1
Davy Nimbona <davynimbona@gmail.com>
2021-01-19 17:48:04 +0100

    Programme

---
+++ chess/__main__.py
@@ -0,0 +1,4 @@
+from tinydb import TinyDB
+
+def main():
+    print("Bienvenue au jeux des échecs\n\n")
---
+++ chess/models/game.py
@@ -0,0 +1,5 @@
+def start_game():
+    print("######## Bienvenue ########")
+
+if __name__ == "__main__":
+    start_game()
