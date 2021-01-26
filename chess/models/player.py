70fe69bfe1ca3954ee96d9e965e2e9d66f9ab7ec
Davy Nimbona <davynimbona@gmail.com>
2021-01-26 01:53:48 +0100

    fonction changer le score du joueur

--- chess/models/player.py
+++ chess/models/player.py
@@ -1,7 +1,11 @@
 class Player():
     def __init__(self, first_name, last_name, birth_date, gender, score):
-        self.f_name = first_name
-        self.l_name = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.date = birth_date
         self.gender = gender
         self.score = score
+
+    @property
+    def change_score(self, new_score):
+        self.score = new_score
