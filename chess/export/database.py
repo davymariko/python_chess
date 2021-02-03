b83af78a39e7a675fab0ec8d246b74078e74bda4
Davy Nimbona <davynimbona@gmail.com>
2021-02-03 21:10:44 +0100

    Export

--- chess/export/database.py
+++ chess/export/database.py
@@ -1,13 +1,21 @@
 from tinydb import TinyDB
 
 
-def initiate_database():
+def initiate_player_table():
     database = TinyDB('database/db.json')
     table = database.table('players')
 
     return table
 
 
+def initiate_game_table():
+    pass
+
+
+def initiate_tournament_table():
+    pass
+
+
 def delete_all_players():
-    players_table = initiate_database()
+    players_table = initiate_player_table()
     players_table.truncate()
