a355c61162fa28148c95c504ec62174b974e29c4
Davy Nimbona <davynimbona@gmail.com>
2021-02-03 11:49:15 +0100

    Revoir l'emplacement des classes et des fonctions

--- chess/controller/controller.py
+++ chess/controller/controller.py
@@ -1,8 +1,8 @@
 from os import system, name
 from time import sleep
-from chess.models.player import Player
+from chess.models.model import Player
 from chess.models.model import (save_player, see_players, number_players,
-                                delete_players, swiss_pair_generator)
+                                delete_all_players, players_list)
 
 
 def start_game():
@@ -11,21 +11,21 @@ def start_game():
     print("1. Commencer un tournoi\n2. Voir joueurs inscrits\
         \n3. Nombre de joueurs inscrits\n4. Effacer tous les joueurs\
         \n5. Rapport\n6. Quitter")
-    jeux = int(input(("\n>>> ")))
-    if jeux == 1:
+    game_choice = int(input(("\n>>> ")))
+    if game_choice == 1:
         start_tournament()
-    elif jeux == 2:
+    elif game_choice == 2:
         clear()
         see_players()
         input("")
         start_game()
-    elif jeux == 3:
+    elif game_choice == 3:
         clear()
         print(f'Nombre de joueurs inscrits: {number_players()}')
         input("")
         start_game()
-    elif jeux == 4:
-        delete_players()
+    elif game_choice == 4:
+        delete_all_players()
         clear()
         print("Tous les joueurs ont été effacés")
         input("")
@@ -38,19 +38,20 @@ def start_tournament():
     tournament_name = input("Nom du tournoi: ")
     venue = input("Lieu: ")
     tournament_date = input("Date du tournoi (Format: jj/mm/aaaa): ")
+    description = input("Description: ")
     print("\n**** Place à la création des joueurs")
     sleep(2)
     generate_players(1)
     clear()
     print("######## Nouveau Tournoi ########\n")
-    print(f"Nom du tournoi: {tournament_name}")
-    print(f"Lieu: {venue}")
-    print(f"Date du tournoi: {tournament_date}")
+    print(f"Nom du tournoi: {tournament_name}\nLieu: {venue}\nDate du\
+tournoi: {tournament_date}\nNombre de joueurs: {number_players()}\
+    \nDescription: {description}")
     print("\n1. Lancer le tournoi\n2. Retour au menu principal")
     players_choice = int(input("\n>>> "))
     if players_choice == 1:
         clear()
-        swiss_pair_generator()
+        generate_pairs()
     elif players_choice == 2:
         print("En sortant de ce tournoi vous l'annulez")
         input("")
@@ -65,17 +66,17 @@ def generate_players(choice):
     if choice == 1:
         check = 0
         number = number_players()
-        while (check < (2-number)):
+        while (check < (8-number)):
             clear()
             remaining_seat = number_players()
             print("########  Créer joueur ########\n")
-            print(f"Place restant: {2-remaining_seat}\n")
+            print(f"Place restant: {8-remaining_seat}\n")
             first_name = input("Prénom: ")
             last_name = input("Nom: ")
             birth_date = input("Date de naissance (Format: jj/mm/aaaa): ")
             gender = input("Sexe(M ou F): ")
             score = 0
-            verify = input("1. Confirmer\n2. Refaire\n3. Retour\n>>> ")
+            verify = input("\n\n1. Confirmer\n2. Refaire\n3. Retour\n>>> ")
             if verify == '1':
                 player = Player(
                     first_name, last_name, birth_date, gender, score)
@@ -85,9 +86,25 @@ def generate_players(choice):
             elif verify == '2':
                 generate_players(1)
             elif verify == '3':
-                start_tournament()
+                start_game()
             check = check + 1
             sleep(1)
+
+
+def generate_pairs():
+    round = 1
+    total_players = number_players()
+    print("Paires généreées pour le Round {1}\n")
+    players = players_list()
+    if round == 1:
+        pairing = 0
+        while pairing < (total_players/2):
+            versus = pairing+int(total_players/2)
+            print(f"\n{players[pairing]['first_name']} {players[pairing]['last_name']}\
+ vs {players[versus]['first_name']} {players[versus]['last_name']}\n")
+            print("========================")
+            pairing += 1
+        input("")
 
 
 def clear():
--- chess/models/model.py
+++ chess/models/model.py
@@ -3,6 +3,48 @@ import pprint
 from time import sleep
 
 
+class Player():
+    def __init__(self, first_name, last_name, birth_date, gender, score):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date = birth_date
+        self.gender = gender
+        self.score = score
+        self.id = (first_name[0].lower() + last_name.lower()+"_" + birth_date.replace("/", ""))
+
+    # def is_valid(self):
+    #     message = ""
+    #     test_dat = self.score
+
+    @property
+    def record(self):
+
+        return {
+                    'id': self.id,
+                    'first_name': self.first_name,
+                    'last_name': self.last_name,
+                    'birth_date': self.date,
+                    'gender': self.gender,
+                    'score': self.score
+                }
+
+
+class Tournoi:
+    def __init__(self, name, venue, date, tours, tournee, players, description):
+
+        self.name = name
+        self.venue = venue
+        self.date = date
+        self.tours = tours
+        self.tournee = tournee
+        self.players = players
+        self.description = description
+
+    @property
+    def define_time(self):
+        print("Hello")
+
+
 def save_player(player_dict):
     database = TinyDB('database/db.json')
     players_table = database.table('players')
@@ -16,6 +58,13 @@ def save_player(player_dict):
     return result
 
 
+def players_list():
+    database = TinyDB('database/db.json')
+    players_table = database.table('players')
+
+    return players_table.all()
+
+
 def see_players():
     database = TinyDB('database/db.json')
     players_table = database.table('players')
@@ -24,10 +73,18 @@ def see_players():
         pprint.pprint(play)
 
 
-def delete_players():
+def delete_all_players():
     database = TinyDB('database/db.json')
     players_table = database.table('players')
     players_table.truncate()
+
+
+# def delete_player():
+#     database = TinyDB('database/db.json')
+#     # players_table = database.table('players')
+#     id = input(("Entrer l'id du joueur à effacer: "))
+#     database.remove(where('id') == id)
+#     print("Le joueur {id} a été effacé")
 
 
 def number_players():
@@ -56,5 +113,13 @@ def launch_tournament():
     pass
 
 
-def play_record():
-    pass
+# def play_record():
+#     database = TinyDB('database/db.json')
+#     play_records = database.table('record')
+
+
+def test():
+    database = TinyDB('database/db.json')
+    test = database.table('players')
+
+    print(test.all()[0]['id'])
